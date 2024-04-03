2ab033b0565dd2263302ef592e65d84f81771740
Teo <dolly@gmail.com>
2024-04-04 01:29:59 +0700

    Style + rendering list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import React, { Component } from 'react';
-import './App.css';
-import Greet from './components/Greet'
-import Welcome from './components/Welcome';
+import React, { Component } from "react";
+import "./App.css";
+import Greet from "./components/Greet";
+import Welcome from "./components/Welcome";
+import { YoutubeData } from "./components/YoutubeData";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App" >
-        <Greet name="Shiba" heroName="Batman">
-          <p>This is children props</p>
-        </Greet>
-        <Greet name="Molly" heroName="Superman">
-          <button>Action</button>
-        </Greet>
-        <Welcome name="Misa" heroName="Wonderwoman"/>
-        <Welcome name="Lala" heroName="Zeus"/>
-      </div>
-    );
-  }
+function Feature() {
+  return (
+    <div className="App">
+      <Greet name="Shiba" heroName="Batman">
+        <p>This is children props</p>
+        <button>Action</button>
+        <h3 className="feature-title">Title</h3>
+        <p className="feature-desc">
+          bvihjlefqripqgheoiffjelnoeislhfskjdflrwjsgkerhgjlkedpowljgilehgljkfssfor
+          egifldbuhrijlkenfjdvuohiljwefphiowefdpjiengrehofwdsipjvfbihegjrpfjdsiegh?
+        </p>
+      </Greet>
+    </div>
+  );
+}
+
+// shift + alt + F -> format
+function App() {
+  console.log({YoutubeData})
+  return (
+    <div className="youtube-list">
+      {YoutubeData.map((item, index) => (
+        <YoutubeItem
+          key={item.id}
+          image={item.image}
+          avatar={item.avatar}
+          title={item.title}
+          author={item.author}
+        ></YoutubeItem>
+      ))} 
+    </div>
+  )
 }
 
 export default App;
+
+function YoutubeItem(props) {
+  return (
+    <div className="youtube-item">
+      <div
+        className="youtube-image"
+        // style={{height: '200px'}}
+      >
+        <img src={props.image1} alt="" />
+      </div>
+      <div className="youtube-footer">
+        <img src={props.image2} alt="" className="youtube-avatar" />
+        <div className="youtube-info">
+          <h3 className="youtube-item youtube-title">{props.title}</h3>
+          <h4 className="youtube-author">{props.author}</h4>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/YoutubeData.js
@@ -0,0 +1,23 @@
+export const YoutubeData = [
+    {
+        id: 1,
+        title: 'Images associées d’une chaîne de',
+        author: 'Zoshua Colah',
+        image: 'https://plus.unsplash.com/premium_photo-1712141088184-061b87d773aa?q=80&w=992&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        avatar: 'https://plus.unsplash.com/premium_photo-1673264933051-3206029946b3?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
+    {
+        id: 2,
+        title: 'Images associées d’une chaîne de',
+        author: 'Zoshua Colah',
+        image: 'https://plus.unsplash.com/premium_photo-1712141088184-061b87d773aa?q=80&w=992&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        avatar: 'https://plus.unsplash.com/premium_photo-1673264933051-3206029946b3?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
+    {
+        id: 3,
+        title: 'Images associées d’une chaîne de',
+        author: 'Zoshua Colah',
+        image: 'https://images.unsplash.com/photo-1647977021867-7c5adf366da6?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        avatar: 'https://plus.unsplash.com/premium_photo-1673264933051-3206029946b3?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
+];
