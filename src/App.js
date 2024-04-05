ecaf19ac2cb1af1c729dcd309386c0cd693d4768
Teo <dolly@gmail.com>
2024-04-05 07:14:24 +0700

    Rendering list 3

--- src/App.js
+++ src/App.js
@@ -4,40 +4,28 @@ import Greet from "./components/Greet";
 import Welcome from "./components/Welcome";
 import { YoutubeData } from "./components/YoutubeData";
 
-function Feature() {
+// shift + alt + F -> format
+function App() {
+  console.log({ YoutubeData });
   return (
-    <div className="App">
-      <Greet name="Shiba" heroName="Batman">
-        <p>This is children props</p>
-        <button>Action</button>
-        <h3 className="feature-title">Title</h3>
-        <p className="feature-desc">
-          bvihjlefqripqgheoiffjelnoeislhfskjdflrwjsgkerhgjlkedpowljgilehgljkfssfor
-          egifldbuhrijlkenfjdvuohiljwefphiowefdpjiengrehofwdsipjvfbihegjrpfjdsiegh?
-        </p>
-      </Greet>
+    <div className="youtube-list">
+      {YoutubeData.map((item, index) => {
+        if (index === 1) {
+          console.log("middle item");
+        }
+        return (
+          <YoutubeItem
+            key={item.id}
+            image={item.image || item.avatar}
+            avatar={item.avatar}
+            title={item.title}
+            author={item.author || "Dolly Kattier"}
+          ></YoutubeItem>
+        );
+      })}
     </div>
   );
 }
-
-// shift + alt + F -> format
-function App() {
-  console.log({YoutubeData})
-  return (
-    <div className="youtube-list">
-      {YoutubeData.map((item, index) => (
-        <YoutubeItem
-          key={item.id}
-          image={item.image}
-          avatar={item.avatar}
-          title={item.title}
-          author={item.author}
-        ></YoutubeItem>
-      ))} 
-    </div>
-  )
-}
-
 export default App;
 
 function YoutubeItem(props) {
@@ -47,10 +35,10 @@ function YoutubeItem(props) {
         className="youtube-image"
         // style={{height: '200px'}}
       >
-        <img src={props.image1} alt="" />
+        <img src={props.image} alt="" />
       </div>
       <div className="youtube-footer">
-        <img src={props.image2} alt="" className="youtube-avatar" />
+        <img src={props.avatar} alt="" className="youtube-avatar" />
         <div className="youtube-info">
           <h3 className="youtube-item youtube-title">{props.title}</h3>
           <h4 className="youtube-author">{props.author}</h4>
