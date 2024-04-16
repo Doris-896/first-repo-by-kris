f885c563f1790d1ed00afadf1e8b5cd424720e8f
Teo <dolly@gmail.com>
2024-04-17 00:01:15 +0700

    final :)))

--- src/components/tictactoe/Game.js
+++ src/components/tictactoe/Game.js
@@ -24,7 +24,7 @@ const Game = () => {
   return (
     <div>
       <Board cells={board} onClick={handleClick}></Board>
-      {winner && <div className="game-winner">Winner is {winner}</div>}
+      {winner && <div className="game-winner">Winner is {winner}</div>}     {/*nếu có winner thì mới hiện*/}
 
       <button className="game-reset" onClick={handleResetGame}>
         Reset game
