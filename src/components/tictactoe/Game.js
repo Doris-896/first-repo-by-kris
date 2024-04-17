4fb19538d9378b0b9ee11bd686ca5ff8a58034f2
Teo <dolly@gmail.com>
2024-04-18 01:21:56 +0700

    state phức tạp

--- src/components/tictactoe/Game.js
+++ src/components/tictactoe/Game.js
@@ -5,25 +5,37 @@ import { calculateNewValue } from "@testing-library/user-event/dist/utils";
 import { calculateWinner } from "../../helpers";
 
 const Game = () => {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
-  const winner = calculateWinner(board);
+  // const [board, setBoard] = useState(Array(9).fill(null));
+  // const [xIsNext, setXIsNext] = useState(true);
+  const[state, setState] = useState({       //gộp về còn 1 biến, truyền vào object
+    board: Array(9).fill(null),             
+    xIsNext: true,
+    name: 'Dolly'
+  });
+  const winner = calculateWinner(state.board);    //chỉnh lại biến truyền vào
   const handleClick = (index) => {
-    const boardCopy = [...board]; 
+    const boardCopy = [...state.board];           //chỉnh lại biến truyền vào
     if (winner || boardCopy[index]) return; 
-    boardCopy[index] = xIsNext ? "X" : "O"; 
+    boardCopy[index] = state.xIsNext ? "X" : "O"; //chỉnh lại biến truyền vào
     console.log(boardCopy[index]);
-    setBoard(boardCopy); 
-    setXIsNext((xIsNext) => !xIsNext); 
+    setState({
+      ...state,
+      board: boardCopy,
+      xIsNext: !state.xIsNext,                     //chỉnh lại biến truyền vào
+    });
+    console.log(state)
+    // setBoard(boardCopy); 
+    // setXIsNext((xIsNext) => !xIsNext); 
   };
 
   const handleResetGame = () => {
-    setBoard(Array(9).fill(null));
+    // setBoard(Array(9).fill(null));
+    // setXIsNext(true);
   };
 
   return (
     <div>
-      <Board cells={board} onClick={handleClick}></Board>
+      <Board cells={state.board} onClick={handleClick}></Board>
       {winner && <div className="game-winner">Winner is {winner}</div>}     {/*nếu có winner thì mới hiện*/}
 
       <button className="game-reset" onClick={handleResetGame}>
